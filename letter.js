635faff491751fbc4c58f21b0a760a864f879b71
tindoann <tindoann@gamil.com>
2019-08-18 20:26:20 -0400

    added pseudocode and comments for word.js

--- letter.js
+++ letter.js
@@ -4,6 +4,7 @@ display an underlying character or a blank placeholder (such as an underscore),
 depending on whether or not the user has guessed the letter. That means the constructor
  should define: */
 
+ // Constructor for letters
  const Letter = function(char) {
 // * A string value to store the underlying character for the letter
   this.char = char; 
@@ -12,10 +13,14 @@ depending on whether or not the user has guessed the letter. That means the cons
   this.guessed = false; 
 /*  * A function that returns the underlying character if the letter has been guessed, 
   or a placeholder (like an underscore) if the letter has not been guessed
+
+  // Method to displays the character or underscore
 */this.display = function() {
+ // If guess is true, return the character
    if (this.guessed){
-     return this.char
+     return this.char.toLowerCase(); 
    } 
+ // else, return an underscore
    else {
      return '_'
    }
@@ -23,7 +28,11 @@ depending on whether or not the user has guessed the letter. That means the cons
 /*  * A function that takes a character as an argument and checks it against the underlying 
   character, updating the stored boolean value to true if it was guessed correctly
 */
+
+// Method to check if the input is the same theis character 
   this.Checker = function (guess) {
+
+    // Switches guessed to true if guess matches this characther 
     if (guess.toLowerCase() === this.char.toLowerCase()) {
       this.guessed = true;
     } else {
@@ -32,4 +41,5 @@ depending on whether or not the user has guessed the letter. That means the cons
   }; 
 }; 
 
+// Exporting the Letter constructor to use in word.js
 module.exports = Letter; 
